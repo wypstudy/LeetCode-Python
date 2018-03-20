889023dac242a299271c39475eb7cff539c1d3a3
wei.yiping <wei.yiping@frontsurf.com>
2018-03-20 19:13:00 +0800

    LeetCode 8 模拟，考虑各种边界条件

---
+++ Algorithms/8.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        number = 0
+        try:
+            length = len(str)
+            start = 0
+            while start < length and str[start] == ' ':
+                start += 1
+            end = start
+            if str[end] == '-' or str[end] == '+':
+                end += 1
+            while end < length and str[end] in "0123456789":
+                end += 1
+            number = int(str[start:end])
+        except Exception:
+            pass
+        if number > 2147483647:
+            return 2147483647
+        elif number < -2147483648:
+            return -2147483648
+        else:
+            return number
+
+
+# test
+instance = Solution()
+print instance.myAtoi("-")
+print instance.myAtoi("")
+print instance.myAtoi("+120a23")
