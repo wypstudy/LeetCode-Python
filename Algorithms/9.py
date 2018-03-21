fc2805a764d9c0d9dfccc31d6d326332545be4ed
wei.yiping <wei.yiping@frontsurf.com>
2018-03-21 18:35:35 +0800

    LeetCode 9 模拟，考虑负整数边界

---
+++ Algorithms/9.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        if int(str(x)[::-1]) == x:
+            return True
+        return False
+# test
+instance = Solution()
+print instance.isPalindrome(-212)
+print instance.isPalindrome(1234321)
+print instance.isPalindrome(1211)
