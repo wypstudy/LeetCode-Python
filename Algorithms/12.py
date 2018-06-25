57d237f495327f1dc0dbc58d68729b148eac4ece
wei.yiping <wei.yiping@frontsurf.com>
2018-06-25 15:24:31 +0800

    LeetCode 12:打表

---
+++ Algorithms/12.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        # 打表
+        thousand_digits = ['', 'M', 'MM', 'MMM']
+        hundred_digits = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+        ten_digits = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+        digits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+        a = num / 1000
+        num = num % 1000
+        b = num / 100
+        num = num % 100
+        c = num / 10
+        d = num % 10
+        roman = thousand_digits[a] + hundred_digits[b] + ten_digits[c] + digits[d]
+        return roman
+
+
+instance = Solution()
+print instance.intToRoman(1)
+print instance.intToRoman(4)
+print instance.intToRoman(9)
+print instance.intToRoman(49)
+print instance.intToRoman(1994)
