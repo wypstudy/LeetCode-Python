86eba62e3c73bc5ee435368b0b509d19c7b0a011
韦一平 <wei.yiping@frontsurf.com>
2019-09-26 18:49:32 +0800

    LeetCode 1:改成python3的排序key函数

--- Algorithms/1.py
+++ Algorithms/1.py
@@ -11,26 +11,26 @@ class Solution(object):
         """
         length = len(nums)
         # 变成[(index,value)]格式
-        nums = [(i, nums[i])for i in range(length)]
+        nums = [(i, nums[i]) for i in range(length)]
         # 根据value排序
-        nums.sort(cmp=lambda x, y: cmp(x[1], y[1]))
+        nums.sort(key=lambda x: x[1])
         # 遍历两个数的组合
         i = 0
         j = 1
         while True:
             a = nums[i][1]
             b = nums[j][1]
-            sumAB = a + b
-            if sumAB == target:
+            sum_ab = a + b
+            if sum_ab == target:
                 return [nums[i][0], nums[j][0]]
             j += 1
             # 当sumAB比target大时，后续不需要加了，直接换下一个a
-            if j == length or sumAB > target:
+            if j == length or sum_ab > target:
                 i += 1
                 j = i + 1
 
 
 # test
 instance = Solution()
-print(instance.twoSum(nums=[2, 5, 5, 11], target=10))
+print(instance.twoSum(nums=[11, 5, 2, 5], target=10))
 
